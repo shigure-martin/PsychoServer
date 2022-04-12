e59b2c27ead7a57ba3930dc5646742585fb394e1
ShigureMartin <363354891@qq.com>
2022-04-12 23:43:40 +0800

    修改getme接口的bug

--- src/main/java/com/psychoServer/constants/RoleType.java
+++ src/main/java/com/psychoServer/constants/RoleType.java
@@ -1,7 +1,7 @@
 package com.psychoServer.constants;
 
 public enum RoleType {
-    customer("用户"), counselor("咨询师"), supervisor("督导");
+    customer("用户"), counselor("咨询师"), supervisor("督导"), admin("管理员");
     public String role;
 
     RoleType(String r) {
